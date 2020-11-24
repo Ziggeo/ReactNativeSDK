c9a4132cc9e54a150e7424c3c25a371f9cad13fe
Severyn <severyn@ziggeo.com>
2020-11-24 09:29:19 -0500

    Added a new class RCTVideos

---
+++ ios/RCTVideos.h
@@ -0,0 +1,16 @@
+#ifndef ZiggeoRCTVideos_h
+#define ZiggeoRCTVideos_h
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <Ziggeo/Ziggeo.h>
+#import <UIKit/UIKit.h>
+
+
+@interface RCTVideos : RCTEventEmitter <RCTBridgeModule>
+
+
+@end
+
+#endif /* ZiggeoRCTVideos_h */
+
---
+++ ios/RCTVideos.m
@@ -0,0 +1,17 @@
+#import <Foundation/Foundation.h>
+#import "RCTVideos.h"
+#import <Ziggeo/Ziggeo.h>
+#import <React/RCTLog.h>
+#import "RotatingImagePickerController.h"
+
+@implementation RCTVideos {
+}
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(destroy:(NSString *)tokenOrKey)
+{
+}
+
+@end
+
